9ee01c933b5dc75f40d8129a57009e84418e379d
peca <peca@g745>
2024-06-06 16:32:02 +0200

    Initial

---
+++ src/rpi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : rpi.cpp
+// Author      : z
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hey hey" << endl; // prints Hey hey
+	return 0;
+}
